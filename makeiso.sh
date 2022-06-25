86dd98e132f07e218644148a9ccfe906bb4766b7
alchg <alchg.pub@gmail.com>
2022-06-25 12:19:27 +0900

    Modified screen setup menu.

--- makeiso.sh
+++ makeiso.sh
@@ -180,6 +180,7 @@ sed -i '/^)/i\ \ ["/usr/local/sbin/alchg.sh"]="0:0:700"' ./archlive/profiledef.s
 sed -i '/^)/i\ \ ["/usr/local/sbin/cowspace.sh"]="0:0:700"' ./archlive/profiledef.sh
 sed -i '/^)/i\ \ ["/etc/skel/alchg/conky.sh"]="0:0:755"' ./archlive/profiledef.sh
 sed -i '/^)/i\ \ ["/etc/skel/alchg/conky_transparent.sh"]="0:0:755"' ./archlive/profiledef.sh
+sed -i '/^)/i\ \ ["/etc/skel/alchg/lxrandr.sh"]="0:0:755"' ./archlive/profiledef.sh
 sed -i '/^)/i\ \ ["/etc/skel/alchg/exec.sh"]="0:0:755"' ./archlive/profiledef.sh
 sed -i '/^)/i\ \ ["/etc/skel/alchg/menu.sh"]="0:0:755"' ./archlive/profiledef.sh
 sed -i '/^)/i\ \ ["/etc/skel/alchg/pipe_menu.sh"]="0:0:755"' ./archlive/profiledef.sh
@@ -663,6 +664,15 @@ fi
 
 EOF
 
+cat >./archlive/airootfs/etc/skel/alchg/lxrandr.sh<<"EOF"
+#! /bin/bash
+
+lxrandr
+echo "" >>/home/user/.config/conky/conky.conf
+nitrogen --restore
+EOF
+
+
 mkdir -p ./archlive/airootfs/usr/share/icons/hicolor/64x64/apps/
 until curl -L -Y 10240 -C - --limit-rate 10240K -k -o "./archlive/airootfs/usr/share/icons/hicolor/64x64/apps/utilities-terminal.png" "https://icon-icons.com/downloadimage.php?id=34340&root=317/PNG/64/&file=terminal-icon_34340.png";do date;echo RETRY;done
 
@@ -907,7 +917,7 @@ cat >./archlive/airootfs/etc/skel/.config/openbox/menu_jp.xml<<"EOF"
   </item>
   <item label="画面">
     <action name="Execute">
-     <command>lxrandr</command>
+     <command>/home/user/alchg/lxrandr.sh</command>
     </action>
   </item>
   <item label="日本語入力">
@@ -1040,7 +1050,7 @@ cat >./archlive/airootfs/etc/skel/.config/openbox/menu_en.xml<<"EOF"
   </item>
   <item label="Screen">
     <action name="Execute">
-     <command>lxrandr</command>
+     <command>/home/user/alchg/lxrandr.sh</command>
     </action>
   </item>
   <item label="TaskBar">
